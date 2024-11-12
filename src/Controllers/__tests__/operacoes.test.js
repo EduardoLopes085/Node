798133732d4c90e92ca2a2a5e820f3ca5c4a3a37
eduardo-lopes <eduardolcb18@gmail.com>
2024-11-12 16:56:16 -0300

    teste de um controller

--- src/Controllers/__tests__/operacoes.test.js
+++ src/Controllers/__tests__/operacoes.test.js
@@ -1,12 +1,24 @@
+const operacoes = require('../operacoesController')
+
 test('1+2 o resultado será 3', ()=>{
     const resultado = 3;
-    expect(1+2).toBe(resultado)
+    expect(operacoes.soma(1, 2)).toBe(resultado)
 })
 
+test('1+2 o resultado será 3', ()=>{
+    const resultado = 8;
+    expect(operacoes.sub(10, 2)).toBe(resultado)
+})
 
+test('1+2 o resultado será 3', ()=>{
+    const resultado = 20;
+    expect(operacoes.mult(10, 2)).toBe(resultado)
+})
 
-
-
+test('1+2 o resultado será 3', ()=>{
+    const resultado = 5;
+    expect(operacoes.div(10, 2)).toBe(resultado)
+})
 
 
 
